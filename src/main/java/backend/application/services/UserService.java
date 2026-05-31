fa029b3b3814a8218fc61a640ae99035d1e2072b
Iranildo Sousa <iranildosousa1234@gmail.com>
2026-05-30 22:20:24 -0300

    feat(user): adiciona criptografia de senha na criação do usuário

--- src/main/java/backend/application/services/UserService.java
+++ src/main/java/backend/application/services/UserService.java
@@ -4,8 +4,10 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import backend.application.DTO.UserCreateDTO;
 import backend.application.DTO.UserResponseDTO;
 import backend.application.exceptions.ResourceNotFoundException;
 import backend.application.exceptions.UnauthorizedException;
@@ -17,18 +19,20 @@ import backend.application.repositories.UserRepository;
 public class UserService {
     
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository){
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder){
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    public UserResponseDTO create(UserResponseDTO dto){
+    public UserResponseDTO create(UserCreateDTO dto){
         UserModel user = new UserModel();
 
         user.setEmail(dto.email());
         user.setName(dto.name());
-        user.setPassword(dto.password());
-        user.setRole(dto.role());
+        user.setPassword(passwordEncoder.encode(dto.password()));
+        user.setRole(UserRole.USER);
 
         return UserResponseDTO.from(userRepository.save(user));
     }
